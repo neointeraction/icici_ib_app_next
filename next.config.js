7a7b33eca566061d9d8686440e9b46fda9b47ccd
sg-Oranj <85916343+sg-Oranj@users.noreply.github.com>
2022-04-14 13:36:04 +0530

    added security headers except csp

--- next.config.js
+++ next.config.js
@@ -10,4 +10,42 @@ module.exports = withImages({
     buildActivity: false,
   },
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'SAMEORIGIN'
+            },
+            // {
+            //   key: 'Content-Security-Policy',
+            //   value: 'default-src *'
+            // },
+            {
+              key: 'X-XSS-Protection',
+              value: '1; mode=block'
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff'
+            },
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=63072000; includeSubDomains; preload'
+            }  
+        ]
+      },
+    ]
+  },
+
+
+
 });
+
+// const ContentSecurityPolicy = `
+//   default-src 'self' localhost:* ws://localhost:*;
+// `
